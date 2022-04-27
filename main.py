46465bae2978a47a059decd255865392d4fe0b57
Vincenzo <vincen85@gmail.com>
2022-04-27 12:24:18 +0200

    Corrección de errores

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import time
 
 def cotizacion_en_euros(moneda):
     url = "https://api.coinbase.com/v2/exchange-rates?currency=%s" % moneda
-    r = requests.get()
+    r = requests.get(url)
     valor = r.json()["data"]["rates"]["EUR"]
-    print "{%s}: {%s}" % (moneda, valor)
+    print("{%s}: {%s}" % (moneda, valor))
 
 
 while(True):
